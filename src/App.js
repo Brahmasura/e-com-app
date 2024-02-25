0df352a4fe60cededcaf68a21a621a2cc15d9e53
Brahmasura <ashlincds@gmail.com>
2024-02-25 19:36:32 +0530

    create checkout page with checkout item, and cart functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./Routes/navigation/navigation.component";
 import Home from "./Routes/home/home.component";
 import Authentication from "./Routes/authentication/authentication.component";
 import Shop from "./Routes/shop/shop.component";
+import Checkout from "./Routes/checkout/checkout.component";
 
 
 
@@ -14,6 +15,7 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="shop" element={<Shop/>} />
           <Route path="auth" element={<Authentication/>} />
+          <Route path="checkout" element={<Checkout/>} />
         </Route>
       </Routes>
     </>
